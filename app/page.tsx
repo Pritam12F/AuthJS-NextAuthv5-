cf2a9e586eb38f5de9419b80fc5626700e21f3f5
PritamDas <pritam.das.santu@gmail.com>
2024-04-30 16:12:47 +0530

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/login-button";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 export default function Home() {
-  return <div className="bg-red-500">Hi there</div>;
+  return (
+    <main className="h-full justify-center items-center flex flex-col bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <div className="space-y-6 text-center">
+        <h2
+          className={cn(
+            "text-5xl text-white font-semibold drop-shadow-md",
+            font.className
+          )}
+        >
+          &#128274; Auth
+        </h2>
+        <p className="text-2xl">Login with your credentials</p>
+        <LoginButton>
+          <Button size={"lg"}>Sign in</Button>
+        </LoginButton>
+      </div>
+    </main>
+  );
 }
