10f99b98be9ad5e5b98984280017d71bd494d3ae
PritamDas <pritam.das.santu@gmail.com>
2024-05-01 16:15:50 +0530

    added zod validation

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/auth/login-form";
 
 export default function Signup() {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="h-full flex items-center justify-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
       <LoginForm typeofform="Sign In" />
     </div>
   );
